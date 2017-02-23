a66d018fe327ce02339cc080b6c8b21e33824f92
Kevin McCall <kmccall@appacademy.io>
2017-02-22 16:52:34 -0800

    initial commit

---
+++ lib/00_hello.rb
@@ -0,0 +1,12 @@
+# Hey! We'll start out with a few very simple exercises.
+# Please don't skip these, even if they seem trivial;
+# they will also teach you the expected workflow, and how
+# to test your code with rspec.
+
+def hello
+  "oh hey there"
+end
+
+def greet(someone)
+  "Hello, #{soneone}!"
+end
